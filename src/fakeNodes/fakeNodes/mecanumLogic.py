5ea32fa858bc4162debd5ed44ea1b601467dd078
Philip Smith <philip@randomsmiths.com>
2024-11-12 13:33:55 -0500

    update names and add checking logic in

--- src/fakeNodes/fakeNodes/mecanumLogic.py
+++ src/fakeNodes/fakeNodes/mecanumLogic.py
@@ -2,70 +2,74 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist, Vector3
-from sensor_msgs.msg import JointState
+from control_msgs.msg import JointJog
 
 
-class MinimalPublisher(Node):
-
+class mecanumLogic(Node):
     def __init__(self):
-        super().__init__('minimal_publisher')
+        super().__init__('mecanum_logic_tester')
         self.cmd_vel_publisher = self.create_publisher(Twist, 'cmd_vel', 10)
-        self.joint_state_subscriber = self.create_subscription(JointState, 'joint_states', self.joint_state_callback, 10)
+        self.joint_state_subscriber = self.create_subscription(JointJog, 'joint_jog', self.joint_jog_callback, 10)
 
 
         timer_period = 5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.i = -1
 
         #declare messages
         self.cmd0 = Twist() #do nothing
 
         self.cmd1 = Twist() #drive forward
-        self.cmd1.linear = Vector3()
         self.cmd1.linear.x = 1.0
 
         self.cmd2 = Twist() #drive left
-        self.cmd2.linear = Vector3()
         self.cmd2.linear.y = 1.0
 
         self.cmd3 = Twist() #drive right and backwards
-        self.cmd3.linear = Vector3()
         self.cmd3.linear.x = -0.5
         self.cmd3.linear.y = -0.5
 
         self.cmd4 = Twist() #rotate counterclockwise
-        self.cmd4.linear = Vector3()
-        self.cmd4.angular = Vector3()
         self.cmd4.angular.z = 0.5
 
 
     def timer_callback(self):
-
-        match (self.i % 5):
-            case 0:
-                self.cmd_vel_publisher.publish(self.cmd0)
-            case 1:
-                self.cmd_vel_publisher.publish(self.cmd1)
-            case 2:
-                self.cmd_vel_publisher.publish(self.cmd2)
-            case 3:
-                self.cmd_vel_publisher.publish(self.cmd3)
-            case 4:
-                self.cmd_vel_publisher.publish(self.cmd4)
-        
+        cmd = None
         self.i += 1
+        
+        match (self.i % 5):
+            case 0: cmd = self.cmd0
+            case 1: cmd = self.cmd1
+            case 2: cmd = self.cmd2
+            case 3: cmd = self.cmd3
+            case _: cmd = self.cmd4
+        self.cmd_vel_publisher.publish(cmd)
 
-    def joint_state_callback(self, msg):
-        pass
-
-    
-            
+    def joint_jog_callback(self, msg:JointJog):
+        velos = {joint:velocity for joint, velocity in zip(msg.joint_names, msg.velocities)}
+        fl = velos["front_left_wheel"]
+        fr = velos["front_right_wheel"]
+        bl = velos["back_left_wheel"]
+        br = velos["back_right_wheel"]
+        match (self.i % 5):
+            case 0: #do nothing
+                assert(fl == fr == bl == br == 0, f"Wheels are moving when they shouldnt be {fl} {fr} {bl} {br}")
+            case 1: #drive forward
+                assert(fl >= 0 and fr >= 0 and bl >= 0 and br >= 0, f"{fl} {fr} {bl} {br}")
+            case 2: #drive left
+                assert(fl <= 0 and fr >= 0 and bl >= 0 and br <= 0, f"not sure the signs are right here {fl} {fr} {bl} {br}")
+            case 3: #drive right and backwards
+                assert(fl == 0 and fr <= 0 and bl <= 0 and br == 0, f"{fl} {fr} {bl} {br}")
+            case 4: #rotate counterclockwise
+                assert(fl <= 0 and fr >= 0 and bl <= 0 and br >= 0, f"{fl} {fr} {bl} {br}")
+            case _:
+                assert(False, "something went wrong with cases")
 
 
 def main(args=None):
     rclpy.init(args=args)
 
-    minimal_publisher = MinimalPublisher()
+    minimal_publisher = mecanumLogic()
 
     rclpy.spin(minimal_publisher)
 
